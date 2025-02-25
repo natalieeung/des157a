840e2fbf520377562baf02baf7502cfc08ba1c6e
Natalie Ung <nungnungswim@gmail.com>
2025-02-25 14:15:38 -0800

    script

--- everypicture/final-everypicture/script.js
+++ everypicture/final-everypicture/script.js
@@ -4,45 +4,46 @@ window.addEventListener('load', function () {
     
     const sections = document.querySelectorAll('section');
     const body = document.querySelector('body');
-    const div = document.querySelector('div');
+    const glasses = document.querySelectorAll('section img');
+    const popup = document.querySelectorAll('.popup');
     let sectionTops = [];
     let pagetop;
     let counter = 0;
     let prevCounter = 1;
-    //let doneResizing
 
+    // Calculate section tops for scroll tracking
     sections.forEach(function (eachSection) {
         sectionTops.push(Math.floor(eachSection.getBoundingClientRect().top) + window.scrollY);
     });
     
     console.log(sectionTops);
 
+    // Scroll event handler
     window.addEventListener('scroll', function () {
         pagetop = window.scrollY + 100;
-        //console.log(pagetop);
 
         if (pagetop > sectionTops[counter]) {
             counter++;
             console.log(`scrolling down ${counter}`);
-        }
-        else if (counter > 1 && pagetop < sectionTops[counter - 1]) {
+        } else if (counter > 1 && pagetop < sectionTops[counter - 1]) {
             counter--;
             console.log(`scrolling up ${counter}`);
         }
-    
-        if (counter != prevCounter) {
-            // do stuff to the page here
-            console.log(`do something for section ${counter}`);
-            /* body.style.backdropFilter = "blur(0px)"; */
 
+        if (counter !== prevCounter) {
             const clarity = `bglens${counter}`;
-            document.querySelector('body').className = clarity;
+            body.className = clarity;
 
             prevCounter = counter;
         }
-
-    
     });
-    
-    
+
+    glasses.forEach((glass, index) => {
+        glass.addEventListener('click', function () {
+            console.log(`Image ${index} clicked`);
+            if (popups[index]) {
+                popups[index].style.display = 'block';
+            }
+        });
+    });
 });
